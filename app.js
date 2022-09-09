a5b4b50f234d893139230ac1d6d3ce9d7bdb13a4
pcheung <pika206@gmail.com>
2022-09-08 22:42:56 -0500

    app.js: remove extra console logs and alerts, remove unnessary reset choices function

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ let choices = ["Lapis", "Scapella", "Papyrus"];
 //* -----------------------------Variables (state)------------------------------
 let myGame = {
     id: 0,
-    // score: 0, //this was moved into the new variable myStats.wins and is no longer used
     computerChoice: null,
     playerChoice: null,
     winner: null,
-    // playerName: null,
 };
 //document game results besides placing it on screen
 let gameHistory = [];
@@ -43,7 +41,6 @@ for (let el of choiceThree) {
 let headers = document.querySelectorAll(".headerBtn");
 for (header of headers) {
     header.addEventListener("click", function () {
-        console.log("clicked");
         let cardContent = this.nextElementSibling;
         cardContent.classList.toggle("collapsed");
         let cardArrow = this.querySelector(".cardIndicator");
@@ -63,7 +60,6 @@ document.querySelector('#btn4').addEventListener('click', resetGame);
 
 //keyboard eventlisteners
 window.addEventListener('keydown', function (e) {
-    console.log(e.key)
     switch (e.key) {
         case '1': pickChoiceOne()
             break;
@@ -89,37 +85,22 @@ function clearMyStats() {
 
 //functions to set your choice
 function pickChoiceOne() {
-    resetChoices();
     myGame.playerChoice = 1;
     myStats.choseOne++;
-    console.log(myStats);
     gameOutcome();
 }
 function pickChoiceTwo() {
-    resetChoices();
     myGame.playerChoice = 2;
     myStats.choseTwo++;
-    console.log(myStats);
     gameOutcome();
 }
 function pickChoiceThree() {
-    resetChoices();
     myGame.playerChoice = 3;
     myStats.choseThree++;
-    console.log(myStats);
     gameOutcome();
 }
 
 
-
-//function to reset everyone's move and the winner in myGame between rounds of play.
-function resetChoices() {
-    let myGame = {
-        computerChoice: null,
-        playerChoice: null,
-        winner: null,
-    };
-}
 
 function createScoreboard() {
     const scoreboard = document.createElement('div');
@@ -133,14 +114,11 @@ function createScoreboard() {
 
 //function that is the main flow once a player makes a choice
 function gameOutcome() {
-    //increment the game id
     myGame.id++;
-    //The computer randomly picks 0,1 or 2 and posts to the myGame
     myGame.computerChoice = randomChooser();
-    //Game Play final step: calculate the winner
     myGame.winner = determineWinner();
-    console.log(myGame);
-    //store the game result
+
+    //! myGameResult is a temporary holder and is pushed to gameHistory
     const myGameResult = {
         id: myGame.id,
         computerChoice: myGame.computerChoice,
@@ -148,13 +126,12 @@ function gameOutcome() {
         winner: myGame.winner,
     };
     gameHistory.push(myGameResult);
-    console.log(gameHistory);
     postScore();
     updateTotalWins();
     checkAchievements();
 }
 
-//function that displays who picked what, who won, and updates the total score.
+
 function postScore() {
     let score = document.createElement('article');
     score.innerText = `Game ${myGame.id} - ${myGame.winner} Won - the computer chose ${choices[myGame.computerChoice - 1]}, you chose ${choices[myGame.playerChoice - 1]}.`;
@@ -169,27 +146,27 @@ function updateTotalWins() {
     totalWins.innerText = myStats.wins;
 }
 
-//function to clear the posted scores and restart the game id count
+
 function resetGame() {
     let winner = document.querySelector('#gameWinner')
     winner.innerText = 'Game not started';
     const scoreboard = document.querySelector('.scoreboard');
     scoreboard.remove();
     createScoreboard();
-    resetChoices();
+    // resetChoices();
     clearAchievements();
     clearMyStats();
 };
 
-//function for the random computer's move
+//!function for the random computer's move
 function randomChooser() {
     return Math.floor(Math.random() * 3 + 1);
 };
 
-//function with the game logic to determine who won.  
-//Game Rules: 1 beats 2, 2 beats 3, 3 beats 1 
+
+//!Game Rules: 1 beats 2, 2 beats 3, 3 beats 1 
 function determineWinner() {
-    //computer ahd player choice are the same
+    //computer and player choice are the same
     if (myGame.computerChoice === myGame.playerChoice) {
         return 'Nobody';
     }
@@ -214,26 +191,24 @@ function determineWinner() {
         myStats.wins++;
         return 'You';
     }
-    // error log for bad input 
-    else { console.log('Cannot determine winner') }
 };
 
 //achievements
 function checkAchievements() {
     if (myStats.wins === 1 && myStats.achievementFirstWin !== true) {
-        alert('Game Achievement: First Victory');
+        // alert('Game Achievement: First Victory');
         const ach = document.querySelector('#ach1');
         ach.innerText = "First Win";
         myStats.achievementFirstWin = true;
     }
     if (myStats.wins === 5 && myStats.achievementFiveWins !== true) {
-        alert('Gane Achievement: Five Victories');
+        // alert('Gane Achievement: Five Victories');
         const ach = document.querySelector('#ach2');
         ach.innerText = "Five Victories";
         myStats.achievementFiveWins = true;
     }
     if (myStats.choseOne === 10) {
-        alert('Game Achievement: I only need one choice');
+        // alert('Game Achievement: I only need one choice');
         const ach = document.querySelector('#ach3');
         ach.innerText = "Choice# 1 - picked 10 times";
         myStats.achievementRockyX = true;
